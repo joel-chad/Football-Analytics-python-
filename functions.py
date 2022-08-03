cf830fd04b06a3ccbbe2931884557f2e3e850ab1
cruz911 <joelchadambuka@gmail.com>
2022-08-03 17:45:15 +0200

    all my football_vizzes

---
+++ functions.py
@@ -0,0 +1,9 @@
+import seaborn as sns
+
+def plot_heatmap(heat_df, teamId, p_type, ax):
+    heat_df = heat_df.copy()
+    heat_df = heat_df.loc[(heat_df['teamId']==teamId) & (heat_df['type']==p_type)]
+    
+    kde = sns.kdeplot(x=heat_df.x, y=heat_df.y, data=heat_df,
+                 shade=True, cmap='magma',
+                 ax=ax)
